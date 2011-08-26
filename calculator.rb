72136386a3c0a18b3d6d12067fbe1b21903708d1
Rodrigo Pestana <digoonrails@gmail.com>
2011-08-26 13:09:10 -0300

    Added feature dividing

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,14 @@
 input = File.read(ARGV[0])
+# input = '6/2'
 
 numbers_to_add = input.split('+').map{ |n| n.to_i }
 numbers_to_sub = input.split('-').map{ |n| n.to_i }
+numbers_to_div = input.split('/').map{ |n| n.to_i }
 
 if numbers_to_add.size > 1
   print numbers_to_add.inject{ |sum, n | sum + n }
 elsif numbers_to_sub.size > 1
   print numbers_to_sub.inject{ |n1, n2| n1 - n2 }
+elsif numbers_to_div.size > 1
+  print numbers_to_div.inject{ |n1, n2| n1 / n2 }
 end
---
+++ features/dividing.feature
@@ -0,0 +1,7 @@
+@wip
+Feature: Dividing
+	
+	Scenario: Dividing 2 numbers
+		Given the input "6/2"
+		When the calculator is run
+		Then the output should be '3'
