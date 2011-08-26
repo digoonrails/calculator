5aee1d3254fded129850d54acff4e6e007d0dbb9
Rodrigo Pestana <digoonrails@gmail.com>
2011-08-26 13:41:49 -0300

    Refatoring code

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,18 @@
-input = File.read(ARGV[0])
-# input = '6/2'
+@input = File.read(ARGV[0])
+# input = '6-2'
 
-numbers_to_add = input.split('+').map{ |n| n.to_i }
-numbers_to_sub = input.split('-').map{ |n| n.to_i }
-numbers_to_div = input.split('/').map{ |n| n.to_i }
+def numbers(operator)
+  @input.split(operator.to_s).map{ |n| n.to_i }
+end
 
-if numbers_to_add.size > 1
-  print numbers_to_add.inject{ |sum, n | sum + n }
-elsif numbers_to_sub.size > 1
-  print numbers_to_sub.inject{ |n1, n2| n1 - n2 }
-elsif numbers_to_div.size > 1
-  print numbers_to_div.inject{ |n1, n2| n1 / n2 }
+operator = /\D/.match(@input)
+case operator.to_s
+when "+"
+  print numbers(operator).inject{ |sum, n | sum + n }
+when "-"                
+  print numbers(operator).inject{ |sum, n | sum - n }
+when "/"                
+  print numbers(operator).inject{ |n1, n2| n1 / n2 }
+else
+  print 'not found operator'
 end
